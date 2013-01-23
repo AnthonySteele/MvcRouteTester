d2c49d5093548b41658eaef9d8d2d71ec0f34773
AnthonySteele <Anthony@PlasticAvalon.org>
2013-01-23 21:00:04 +0000

    Tests on asserts

---
+++ src/MvcRouteTester.Test/AssertsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace MvcRouteTester.Test
+{
+	[TestFixture]
+	public class AssertsTests
+	{
+		[Test]
+		public void IsNullPasses()
+		{
+			Asserts.Null(null, "fail");
+		}
+
+		[Test]
+		public void NotNullPasses()
+		{
+			Asserts.NotNull(new object(), "fail");
+		}
+
+		[Test]
+		public void EqualStringsPass()
+		{
+			Asserts.StringsEqualIgnoringCase("foo", "foo", "fail");
+		}
+
+		[Test]
+		public void DifferentCaseStringsPass()
+		{
+			Asserts.StringsEqualIgnoringCase("foo", "Foo", "fail");
+		}
+	}
+}
