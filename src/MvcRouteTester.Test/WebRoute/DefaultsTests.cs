b738ce01e699949578998e83d05516a532bed962
AnthonySteele <Anthony@PlasticAvalon.org>
2013-02-01 21:23:40 +0000

    Test for defaults in routes Pleased that it works already

---
+++ src/MvcRouteTester.Test/WebRoute/DefaultsTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.Test.WebRoute
+{
+	[TestFixture]
+	public class DefaultsTests
+	{
+		private RouteCollection routes;
+
+		[SetUp]
+		public void MakeRouteTable()
+		{
+			routes = new RouteCollection();
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = 32 });
+		}
+
+		[Test]
+		public void HasRouteWithAllValuesSpecified()
+		{
+			var expectedRoute = new { controller = "Home", action = "Index", id = "42" };
+			RouteAssert.HasRoute(routes, "/home/index/42", expectedRoute);
+		}
+
+		[Test]
+		public void HasRouteWithDefaultId()
+		{
+			var expectedRoute = new { controller = "Home", action = "Index", id = "32" };
+			RouteAssert.HasRoute(routes, "/home/index", expectedRoute);
+		}
+
+		[Test]
+		public void HasRouteWithDefaultActionAndId()
+		{
+			var expectedRoute = new { controller = "Home", action = "Index", id = "32" };
+			RouteAssert.HasRoute(routes, "/home", expectedRoute);
+		}
+		[Test]
+		public void HasRouteWithAllDefaults()
+		{
+			var expectedRoute = new { controller = "Home", action = "Index", id = "32" };
+			RouteAssert.HasRoute(routes, "/", expectedRoute);
+		}
+	}
+}
