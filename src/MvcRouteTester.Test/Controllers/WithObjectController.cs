4f05fc8e9b788ca76704aacf259d194866f671d5
AnthonySteele <Anthony@PlasticAvalon.org>
2013-02-23 18:22:29 +0000

    Trying out a model binding scenario Failing test may help show how this can be handled.

---
+++ src/MvcRouteTester.Test/Controllers/WithObjectController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace MvcRouteTester.Test.Controllers
+{
+	public class InputModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+	public class WithObjectController : Controller
+	{
+		public ActionResult Index(InputModel data)
+		{
+			return new EmptyResult();
+		}
+	}
+}
---
+++ src/MvcRouteTester.Test/WebRoute/ModelBindingTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+using MvcRouteTester.Test.Controllers;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.Test.WebRoute
+{
+	[TestFixture]
+	public class ModelBindingTest
+	{
+		private RouteCollection routes;
+
+		[SetUp]
+		public void MakeRouteTable()
+		{
+			routes = new RouteCollection();
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}/{name}",
+				defaults: new { controller = "Home", action = "Index" });
+		}
+
+		[Test]
+		public void HasRoute()
+		{
+			RouteAssert.HasRoute(routes, "/withobject/index/1/fred");
+		}
+
+		[Test]
+		public void HasRouteToController()
+		{
+			var expectedRoute = new
+			{
+				controller = "withobject",
+				action = "Index"
+			};
+
+			RouteAssert.HasRoute(routes, "/withobject/index/1/fred", expectedRoute);
+		}
+
+		[Test, Ignore("Not passing yet. todo: find a way to do this...")]
+		public void HasRouteCapturesObjectValues()
+		{
+			var data = new InputModel
+				{
+					Id = 1,
+					Name = "fred"
+				};
+			var expectedRoute = new
+				{
+					controller = "withobject", 
+					action = "Index",
+					data = data
+				};
+
+			RouteAssert.HasRoute(routes, "/withobject/index/1/fred", expectedRoute);
+		}
+	}
+}
