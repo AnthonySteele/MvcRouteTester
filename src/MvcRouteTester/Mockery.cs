433ad4bd7e650033884d260d4f881f4d6754a417
Yves Reynhout <yves.reynhout@gmail.com>
2013-03-13 12:31:59 +0100

    Removed Moq dependency
    Removed Moq as a dependency from the MvcRouteTester c# project and its
    package.config, and from the nuspec template.
    Replaced Moq usage by two dedicated classes that do what Moq used to do.

--- src/MvcRouteTester/Mockery.cs
+++ src/MvcRouteTester/Mockery.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
 using System.Web;
-using Moq;
 
 namespace MvcRouteTester
 {
@@ -13,12 +11,58 @@ namespace MvcRouteTester
 			var relativeUrl = routeParts[0];
 			var queryParams = UrlHelpers.MakeQueryParams(url);
 
-			var httpContextMock = new Mock<HttpContextBase>();
-			httpContextMock.Setup(c => c.Request.AppRelativeCurrentExecutionFilePath).Returns(relativeUrl);
-			httpContextMock.Setup(c => c.Request.QueryString).Returns(queryParams);
-			httpContextMock.Setup(c => c.Request.Params).Returns(queryParams);
-			httpContextMock.Setup(c => c.Request.PathInfo).Returns(string.Empty);
-			return httpContextMock.Object;
+			return new PrivateHttpContext(new PrivateHttpRequest(relativeUrl, queryParams));
+		}
+
+		class PrivateHttpContext : HttpContextBase
+		{
+			readonly HttpRequestBase request;
+
+			public PrivateHttpContext(HttpRequestBase request)
+			{
+				this.request = request;
+			}
+
+			public override HttpRequestBase Request {
+				get {
+					return this.request;
+				}
+			}
+		}
+
+		class PrivateHttpRequest : HttpRequestBase
+		{
+			readonly string relativeUrl;
+			readonly NameValueCollection queryParams;
+
+			public PrivateHttpRequest(string relativeUrl, NameValueCollection queryParams)
+			{
+				this.relativeUrl = relativeUrl;
+				this.queryParams = queryParams;
+			}
+
+			public override string AppRelativeCurrentExecutionFilePath {
+				get {
+					return this.relativeUrl;
+				}
+			}
+			public override NameValueCollection QueryString {
+				get {
+					return this.queryParams;
+				}
+			}
+
+			public override NameValueCollection Params {
+				get {
+					return this.queryParams;
+				}
+			}
+
+			public override string PathInfo {
+				get {
+					return string.Empty;
+				}
+			}
 		}
 	}
 }
