7407a0d7758e6f3c5a87b2182fe9642136e1e524
Anthony Steele <Anthony.Steele@7digital.com>
2013-04-08 17:18:24 +0100

    Added FakeAssertEngine to test project will be used for testing scenarios where a test failure is expected

---
+++ src/MvcRouteTester.Test/FakeAssertEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using MvcRouteTester.Assertions;
+
+namespace MvcRouteTester.Test
+{
+	class FakeAssertEngine : IAssertEngine
+	{
+		private readonly IList<string> messages = new List<string>();
+
+		public int FailCount { get; set; }
+		public int StringMismatchCount { get; set; }
+		public int StringMatchCount { get; set; }
+
+		public IList<string> Messages
+		{
+			get { return messages; }
+		}
+
+		public void Fail(string message)
+		{
+			FailCount++;
+			messages.Add(message);
+		}
+
+		public void StringsEqualIgnoringCase(string s1, string s2, string message)
+		{
+			if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2))
+			{
+				StringMatchCount++;
+				return;
+			}
+
+			if (string.Equals(s1, s2, StringComparison.InvariantCultureIgnoreCase))
+			{
+				StringMatchCount++;
+			}
+			else
+			{
+				StringMismatchCount++;
+				messages.Add(message);
+			}
+		}
+	}
+}
