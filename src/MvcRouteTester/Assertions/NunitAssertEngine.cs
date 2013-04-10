3e3b9af7db39df61246acbeda3b893a21c1176aa
AnthonySteele <Anthony@PlasticAvalon.org>
2013-04-10 18:54:24 +0100

    comment in NunitAssertEngine

--- src/MvcRouteTester/Assertions/NunitAssertEngine.cs
+++ src/MvcRouteTester/Assertions/NunitAssertEngine.cs
@@ -9,6 +9,13 @@ namespace MvcRouteTester.Assertions
 			Assert.Fail(message);
 		}
 		
+		/// <summary>
+		/// This could be a wrapper over "Fail" and reduce the size of the  IAssertEngine interface
+		/// But then we would lose out on the error details that StringAssert.AreEqual provides
+		/// </summary>
+		/// <param name="s1"></param>
+		/// <param name="s2"></param>
+		/// <param name="message"></param>
 		public void StringsEqualIgnoringCase(string s1, string s2, string message)
 		{
 			if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2))
