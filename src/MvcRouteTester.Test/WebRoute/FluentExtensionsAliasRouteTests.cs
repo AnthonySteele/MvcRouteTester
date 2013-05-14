169beb7236d0218fcbefb27f70f43ec2f8d72e39
AnthonySteele <Anthony@PlasticAvalon.org>
2013-05-14 22:09:08 +0100

    Tests on capitalization and alias routes, as per issue 8 https://github.com/AnthonySteele/MvcRouteTester/issues/8

---
+++ src/MvcRouteTester.Test/WebRoute/FluentExtensionsAliasRouteTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+using MvcRouteTester.Test.Controllers;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.Test.WebRoute
+{
+	[TestFixture]
+	public class FluentExtensionsAliasRouteTests
+	{
+		[Test]
+		public void FluentRouteWithCaps()
+		{
+			var routesWithCaps = new RouteCollection();
+			routesWithCaps.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			// note that "Controller" and "Action" are capitalised
+			routesWithCaps.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { Controller = "Home", Action = "Index", id = 32 });
+
+			routesWithCaps.ShouldMap("/home/index/32").To<HomeController>(x => x.Index(32));
+		}
+
+		[Test]
+		public void FluentRouteShouldHandleAliasRoute()
+		{
+			var routesWithAlias = new RouteCollection();
+			routesWithAlias.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routesWithAlias.MapRoute(
+				name: "Alias Route",
+				url: "aliasroute",
+				defaults: new { controller = "Home", action = "Index", id = 32 });
+
+			routesWithAlias.ShouldMap("/aliasroute").To<HomeController>(x => x.Index(32));
+		}
+
+		[Test]
+		public void FluentRouteShouldHandleAliasRouteWithCaps()
+		{
+			var routesWithAlias = new RouteCollection();
+			routesWithAlias.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			// note that "Controller" and "Action" are capitalised
+			routesWithAlias.MapRoute(
+				name: "Alias Route",
+				url: "aliasroute",
+				defaults: new { Controller = "Home", Action = "Index", id = 32 });
+
+			routesWithAlias.ShouldMap("/aliasroute").To<HomeController>(x => x.Index(32));
+		}
+	}
+}
---
+++ src/MvcRouteTester.Test/WebRoute/HasAliasRouteTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.Test.WebRoute
+{
+	public class HasAliasRouteTests
+	{
+		[Test]
+		public void FluentRouteWithCaps()
+		{
+			var routesWithCaps = new RouteCollection();
+			routesWithCaps.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			// note that "Controller" and "Action" are capitalised
+			routesWithCaps.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { Controller = "Home", Action = "Index", id = 32 });
+
+			RouteAssert.HasRoute(routesWithCaps, "/home/index/32", "Home", "Index");
+		}
+
+		[Test]
+		public void FluentRouteShouldHandleAliasRoute()
+		{
+			var routesWithAlias = new RouteCollection();
+			routesWithAlias.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routesWithAlias.MapRoute(
+				name: "Alias Route",
+				url: "aliasroute",
+				defaults: new { controller = "Home", action = "Index", id = 32 });
+
+			RouteAssert.HasRoute(routesWithAlias, "/aliasroute", "Home", "Index");
+		}
+
+		[Test]
+		public void FluentRouteShouldHandleAliasRouteWithCaps()
+		{
+			var routesWithAlias = new RouteCollection();
+			routesWithAlias.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			// note that "Controller" and "Action" are capitalised
+			routesWithAlias.MapRoute(
+				name: "Alias Route",
+				url: "aliasroute",
+				defaults: new { Controller = "Home", Action = "Index", id = 32 });
+
+			RouteAssert.HasRoute(routesWithAlias, "/aliasroute", "Home", "Index");
+		}
+
+	}
+}
