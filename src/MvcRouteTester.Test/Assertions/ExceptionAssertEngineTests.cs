bd78d8ba869bb0d34662165c3ceed826f8a2e50e
AnthonySteele <Anthony@PlasticAvalon.org>
2013-05-25 18:34:13 +0100

    Tests on ExceptionAssertEngine Brought the NunitAssertEngine back, in the test project Formatting

---
+++ src/MvcRouteTester.Test/Assertions/ExceptionAssertEngineTests.cs
@@ -0,0 +1,38 @@
+﻿using MvcRouteTester.Assertions;
+using NUnit.Framework;
+using AssertionException = MvcRouteTester.Assertions.AssertionException;
+
+namespace MvcRouteTester.Test.Assertions
+{
+	[TestFixture]
+	public class ExceptionAssertEngineTests
+	{
+		[Test]
+		public void FailThrowsException()
+		{
+			var assertEngine = new ExceptionAssertEngine();
+			Assert.Throws<AssertionException>(() => assertEngine.Fail("Failed"));
+		}
+
+		[Test]
+		public void StringMismatchThrowsException()
+		{
+			var assertEngine = new ExceptionAssertEngine();
+			Assert.Throws<AssertionException>(() => assertEngine.StringsEqualIgnoringCase("foo", "bar", "Failed"));
+		}
+
+		[Test]
+		public void StringMatchDoesNotThrowException()
+		{
+			var assertEngine = new ExceptionAssertEngine();
+			Assert.DoesNotThrow(() => assertEngine.StringsEqualIgnoringCase("foo", "foo", "Failed"));
+		}
+
+		[Test]
+		public void EmptyAndNullStringsDoesNotThrowException()
+		{
+			var assertEngine = new ExceptionAssertEngine();
+			Assert.DoesNotThrow(() => assertEngine.StringsEqualIgnoringCase(string.Empty, null, "Failed"));
+		}
+	}
+}
---
+++ src/MvcRouteTester.Test/Assertions/NunitAssertEngine.cs
@@ -0,0 +1,30 @@
+﻿using MvcRouteTester.Assertions;
+using NUnit.Framework;
+
+namespace MvcRouteTester.Test.Assertions
+{
+	public class NunitAssertEngine : IAssertEngine
+	{
+		public void Fail(string message)
+		{
+			Assert.Fail(message);
+		}
+
+		/// <summary>
+		/// This could be a wrapper over "Fail" and reduce the size of the  IAssertEngine interface
+		/// But then we would lose out on the error details that StringAssert.AreEqual provides
+		/// </summary>
+		/// <param name="s1"></param>
+		/// <param name="s2"></param>
+		/// <param name="message"></param>
+		public void StringsEqualIgnoringCase(string s1, string s2, string message)
+		{
+			if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2))
+			{
+				return;
+			}
+
+			StringAssert.AreEqualIgnoringCase(s1, s2, message);
+		}
+	}
+}
