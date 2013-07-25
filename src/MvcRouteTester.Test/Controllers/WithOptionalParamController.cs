2a450a49e081543804eaa18c2f2c15dfc71a850f
AnthonySteele <Anthony@PlasticAvalon.org>
2013-07-25 21:53:17 +0100

    Added tests on optional param to controller

---
+++ src/MvcRouteTester.Test/Controllers/WithOptionalParamController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MvcRouteTester.Test.Controllers
+{
+	public class WithOptionalParamController : Controller
+	{
+		public ActionResult Search(string country, string action, string id)
+		{
+			return new EmptyResult();
+		}
+	}
+}
---
+++ src/MvcRouteTester.Test/WebRoute/WithOptionalParamTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+using MvcRouteTester.Test.Assertions;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.Test.WebRoute
+{
+	[TestFixture]
+	public class WithOptionalParamTests
+	{
+		private RouteCollection routes;
+
+		[SetUp]
+		public void MakeRouteTable()
+		{
+			RouteAssert.UseAssertEngine(new NunitAssertEngine());
+
+			routes = new RouteCollection();
+			routes.MapRoute(
+				"CountryId",
+				"{country}/withparams/{action}/{id}",
+				new { controller = "WithOptionalParam", country = "uk", action = "Search", id = UrlParameter.Optional });
+		}
+
+		[Test]
+		public void HasRouteWhenOptionalParamIsSpecified()
+		{
+			RouteAssert.HasRoute(routes, "/uk/withparams/Search/123");
+		}
+
+		[Test]
+		public void MatchesRouteWhenOptionalParamIsSpecified()
+		{
+			RouteAssert.HasRoute(routes, "/uk/withparams/Search/123", new
+				{
+					Controller = "WithOptionalParam",
+					Action = "Search",
+					Id = "123"
+				});
+		}
+
+		[Test]
+		public void HasRouteWhenOptionalParamIsNotSpecified()
+		{
+			RouteAssert.HasRoute(routes, "/uk/withparams/Search/");
+		}
+
+		[Test]
+		public void MatchesRouteWhenOptionalParamIsNotSpecified()
+		{
+			RouteAssert.HasRoute(routes, "/uk/withparams/Search/", new
+			{
+				Controller = "WithOptionalParam",
+				Action = "Search"
+			});
+		}
+
+	}
+}
