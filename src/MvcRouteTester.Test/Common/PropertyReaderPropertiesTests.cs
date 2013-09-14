70ecce4799441a4c105bd4981a3a595994fd38be
AnthonySteele <Anthony@PlasticAvalon.org>
2013-09-14 14:34:45 +0100

    More tests on property read test new code that returns a RouteValues

--- src/MvcRouteTester.Test/Common/PropertyReaderPropertiesTests.cs
+++ src/MvcRouteTester.Test/Common/PropertyReaderPropertiesTests.cs
@@ -118,5 +118,50 @@ namespace MvcRouteTester.Test.Common
 			Assert.That(properties.Count, Is.EqualTo(1));
 			Assert.That(properties.ValueByName("ValueUnderTest"), Is.EqualTo(aGuid));
 		}
+
+		[Test]
+		public void ShouldReadToRouteValues()
+		{
+			var propsIn = new
+				{
+					Foo = 1,
+					Bar = "Two"
+				};
+
+			var routeValues = reader.RouteValues(propsIn);
+
+			Assert.That(routeValues, Is.Not.Null);
+			Assert.That(routeValues.Controller, Is.Null);
+			Assert.That(routeValues.Action, Is.Null);
+			Assert.That(routeValues.Area, Is.Null);
+
+			Assert.That(routeValues.Values.Count, Is.EqualTo(2));
+			Assert.That(routeValues.GetRouteValue("Foo", RouteValueOrigin.Unknown).Value, Is.EqualTo(1));
+			Assert.That(routeValues.GetRouteValue("Bar", RouteValueOrigin.Unknown).Value, Is.EqualTo("Two"));
+		}
+
+		[Test]
+		public void ShouldReadPropertiesToDataOnRouteValues()
+		{
+			var propsIn = new
+				{
+					Controller = "Fred",
+					Action = "Jim",
+					Area = "WhiteZone",
+					Foo = 1, 
+					Bar = "Two"
+				};
+
+			var routeValues = reader.RouteValues(propsIn) ;
+
+			Assert.That(routeValues, Is.Not.Null);
+			Assert.That(routeValues.Controller, Is.EqualTo("Fred"));
+			Assert.That(routeValues.Action, Is.EqualTo("Jim"));
+			Assert.That(routeValues.Area, Is.EqualTo("WhiteZone"));
+
+			Assert.That(routeValues.Values.Count, Is.EqualTo(2));
+			Assert.That(routeValues.GetRouteValue("Foo", RouteValueOrigin.Unknown).Value, Is.EqualTo(1));
+			Assert.That(routeValues.GetRouteValue("Bar", RouteValueOrigin.Unknown).Value, Is.EqualTo("Two"));
+		}
 	}
 }
