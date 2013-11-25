62c21ba09e11c76fadeaca58e782814decfab962
AnthonySteele <Anthony@PlasticAvalon.org>
2013-11-25 22:51:56 +0000

    Started with fluent asserts fail tests

---
+++ src/MvcRouteTester.AttributeRouting.Test/WebRoute/HomeAttrControllerFailTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.AttributeRouting.Test.WebRoute
+{
+	[TestFixture]
+	public class HomeAttrControllerFailTests
+	{
+		private FakeAssertEngine assertEngine;
+
+		[SetUp]
+		public void Setup()
+		{
+			assertEngine = new FakeAssertEngine();
+			RouteAssert.UseAssertEngine(assertEngine);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			RouteAssert.UseAssertEngine(new NunitAssertEngine());
+		}
+	}
+}
