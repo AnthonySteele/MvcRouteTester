08e5454b536839e8db0be33b997d39f8ebd56228
AnthonySteele <Anthony@PlasticAvalon.org>
2013-11-26 20:58:12 +0000

    fail tests on attr routes

---
+++ src/MvcRouteTester.AttributeRouting.Test/NunitAssertEngine.cs
@@ -0,0 +1,28 @@
+﻿using MvcRouteTester.Assertions;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.AttributeRouting.Test
+{
+	public class NunitAssertEngine : IAssertEngine
+	{
+		public void Fail(string message)
+		{
+			Assert.Fail(message);
+		}
+
+		/// <summary>
+		/// This could be a wrapper over "Fail" and reduce the size of the  IAssertEngine interface
+		/// But then we would lose out on the error details that StringAssert.AreEqual provides
+		/// </summary>
+		public void StringsEqualIgnoringCase(string s1, string s2, string message)
+		{
+			if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2))
+			{
+				return;
+			}
+
+			StringAssert.AreEqualIgnoringCase(s1, s2, message);
+		}
+	}
+}
---
+++ src/MvcRouteTester.AttributeRouting.Test/WebRoute/HomeAttrControllerFailTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+using MvcRouteTester.AttributeRouting.Test.Controllers;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.AttributeRouting.Test.WebRoute
+{
+	[TestFixture]
+	public class HomeAttrControllerFailTests
+	{
+		private FakeAssertEngine assertEngine;
+		private RouteCollection routes;
+
+		[SetUp]
+		public void Setup()
+		{
+			assertEngine = new FakeAssertEngine();
+			RouteAssert.UseAssertEngine(assertEngine);
+
+			routes = new RouteCollection();
+			routes.MapMvcAttributeRoutes();
+		}
+
+		[Test]
+		public void TestNameMismatches()
+		{
+			var expectedRoute = new { controller = "HoomAttr", action = "Mindex" };
+			RouteAssert.HasRoute(routes, "/homeattr/index", expectedRoute);
+
+			Assert.That(assertEngine.StringMismatchCount, Is.EqualTo(2));
+			Assert.That(assertEngine.Messages[0], Is.EqualTo("Expected 'HoomAttr', not 'HomeAttr' for 'controller' at url '/homeattr/index'."));
+			Assert.That(assertEngine.Messages[1], Is.EqualTo("Expected 'Mindex', not 'Index' for 'action' at url '/homeattr/index'."));
+		}
+
+		[Test]
+		public void TestWrongNoRoute()
+		{
+			RouteAssert.NoRoute(routes, "/homeattr/index");
+
+			Assert.That(assertEngine.FailCount, Is.EqualTo(1));
+			Assert.That(assertEngine.Messages[0], Is.EqualTo("Should not have found the route to '/homeattr/index'"));
+		}
+
+		[Test]
+		public void TestFluentRouteMismatch()
+		{
+			routes.ShouldMap("/hoomattr/mindex").To<HomeAttrController>(x => x.Index());
+
+			Assert.That(assertEngine.FailCount, Is.GreaterThan(0));
+			Assert.That(assertEngine.Messages[0], Is.EqualTo("Should have found the route to '/hoomattr/mindex'"));
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			RouteAssert.UseAssertEngine(new NunitAssertEngine());
+		}
+	}
+}
