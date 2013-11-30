4f041e20f5dcd76f035cac27a41e9e921a56ca71
AnthonySteele <Anthony@PlasticAvalon.org>
2013-11-30 18:43:22 +0000

    Tests on attribute api routes And fixes for these

---
+++ src/MvcRouteTester.AttributeRouting.Test/ApiRoute/OptionalParamTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+using AttributeRouting.Web.Http.WebHost;
+
+using MvcRouteTester.AttributeRouting.Test.ApiControllers;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.AttributeRouting.Test.ApiRoute
+{
+	[TestFixture]
+	public class OptionalParamTests
+	{
+		private HttpConfiguration config;
+
+		[SetUp]
+		public void MakeRouteTable()
+		{
+			config = new HttpConfiguration();
+			config.Routes.MapHttpAttributeRoutes(c =>
+				{
+					c.InMemory = true;
+					c.AddRoutesFromController<WithOptionalParamAttrController>();
+				});
+		}
+
+		[Test]
+		public void ShouldHaveRouteWithOptionalParamSupplied()
+		{
+			var expectations = new
+				{
+					controller = "WithOptionalParamAttr",
+					action = "Get",
+					paramA = 42,
+					paramB = 34
+				};
+
+			RouteAssert.HasApiRoute(config, "/someapi/withoptionalparam/42?paramB=34", HttpMethod.Get, expectations);
+		}
+
+		[Test]
+		public void ShouldHaveRouteWithOptionalParamSuppliedWithFluentExpectation()
+		{
+			config.ShouldMap("/someapi/withoptionalparam/42?paramB=34")
+				.To<WithOptionalParamAttrController>(HttpMethod.Get, x => x.Get(42, 34));
+		}
+
+		[Test]
+		public void ShouldHaveRouteWithOptionalParamNotSupplied()
+		{
+			var expectations = new
+			{
+				controller = "WithOptionalParamAttr",
+				action = "Get",
+				paramA = 42,
+			};
+
+			RouteAssert.HasApiRoute(config, "/someapi/withoptionalparam/42/", HttpMethod.Get, expectations);
+		}
+
+		[Test]
+		public void ShouldHaveRouteWithOptionalParamNotSuppliedAndExpectationOfNull()
+		{
+			var expectations = new
+				{
+					controller = "WithOptionalParamAttr",
+					action = "Get",
+					paramA = 42,
+					paramB = (int?)null
+				};
+
+			RouteAssert.HasApiRoute(config, "/someapi/withoptionalparam/42/", HttpMethod.Get, expectations);
+		}
+
+		[Test]
+		public void ShouldHaveRouteWithOptionalParamNotSuppliedFluentExpectation()
+		{
+			config.ShouldMap("/someapi/withoptionalparam/42/")
+				.To<WithOptionalParamAttrController>(HttpMethod.Get, x => x.Get(42, null));
+		}
+	}
+}
