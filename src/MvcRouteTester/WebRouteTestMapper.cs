6de4858f3f7e54c1b2dbc2b264cb3e6a2f029aae
AnthonySteele <Anthony@PlasticAvalon.org>
2014-03-05 21:02:27 +0000

    got working in MVC 5.1 using ugly reflection to call method on internal class and new complexity in reading route values

--- src/MvcRouteTester/WebRouteTestMapper.cs
+++ src/MvcRouteTester/WebRouteTestMapper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Web.Http.Filters;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -31,22 +32,47 @@ namespace MvcRouteTester
 
 		public static void MapAttributeRoutesInControllers(this RouteCollection routes, IEnumerable<Type> controllers)
 		{
-			var mapMvcAttributeRoutesMethod = typeof(RouteCollectionAttributeRoutingExtensions)
-				.GetMethod("MapMvcAttributeRoutes",
-				BindingFlags.NonPublic | BindingFlags.Static,
-				null,
-				new[] { typeof(RouteCollection), typeof(IEnumerable<Type>) },
-				null);
+			var attributeRoutingMapperType =  GetInternalMapperType();
+
+			if (attributeRoutingMapperType == null)
+			{
+				const string MissingTypeMessage = "Internal type System.Web.Mvc.Routing.AttributeRoutingMapper not found. " +
+					"You may have updated ASP MVC to a version later than 5.1.1 " +
+					" Check online for a new version of MvcRouteTester";
+				throw new ApplicationException(MissingTypeMessage);
+			}
+
+			var mapMvcAttributeRoutesMethod = GetMapRoutesMethod(attributeRoutingMapperType);
 
 			if (mapMvcAttributeRoutesMethod == null)
 			{
 				const string MissingMethodMessage = "Internal method System.Web.Mvc.RouteCollectionAttributeRoutingExtensions.MapMvcAttributeRoutes not found. " +
-					"You may have updated ASP MVC to a version later than 5.0. " +
-					" Check online fir a new version of MvcRouteTester";
+					"You may have updated ASP MVC to a version later than 5.1.1 " +
+					" Check online for a new version of MvcRouteTester";
 				throw new ApplicationException(MissingMethodMessage);
 			}
 
 			mapMvcAttributeRoutesMethod.Invoke(null, new object[] { routes, controllers });
+		}
+
+		private static Type GetInternalMapperType()
+		{
+			var mvcAssembly = Assembly.Load("System.Web.Mvc, Version=5.1.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+			if (mvcAssembly == null)
+			{
+				return null;
+			}
+			return mvcAssembly.GetType("System.Web.Mvc.Routing.AttributeRoutingMapper");
+		}
+
+		private static MethodInfo GetMapRoutesMethod(Type attributeRoutingMapperType)
+		{
+			return attributeRoutingMapperType.GetMethod(
+				"MapAttributeRoutes",
+				BindingFlags.Public | BindingFlags.Static,
+				null,
+				new[] { typeof(RouteCollection), typeof(IEnumerable<Type>) },
+				null);
 		}
 	}
 }
