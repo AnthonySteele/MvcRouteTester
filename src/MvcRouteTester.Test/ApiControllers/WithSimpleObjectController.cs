ac3f9c4b3f45382e5023a51c2724b5da6a32312f
aidinabedi <aidinabedi@gmail.com>
2015-03-10 12:34:10 +0100

    Added tests for custom simple types.

---
+++ src/MvcRouteTester.Test/ApiControllers/WithSimpleObjectController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcRouteTester.Test.ApiControllers
+{
+	[TypeConverter(typeof(SimpleInputModelConverter))]
+	public class SimpleInputModel
+	{
+		public uint X { get; set; }
+		public uint Y { get; set; }
+
+		public override string ToString()
+		{
+			return X + "-" + Y;
+		}
+
+		public static SimpleInputModel Parse(string value)
+		{
+			var elements = value.Split('-');
+
+			return new SimpleInputModel
+			{
+				X = UInt32.Parse(elements[0]),
+				Y = UInt32.Parse(elements[1])
+			};
+		}
+	}
+
+	class SimpleInputModelConverter : TypeConverter
+	{
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+		}
+
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			var str = value as string;
+			if (str != null)
+			{
+				return new InputModel { Name = str };
+			}
+
+			return base.ConvertFrom(context, culture, value);
+		}
+	}
+
+	public class WithSimpleObjectController : ApiController
+	{
+		public HttpResponseMessage Get(SimpleInputModel data)
+		{
+			return new HttpResponseMessage();
+		}
+	}
+}
---
+++ src/MvcRouteTester.Test/ApiRoute/SimpleModelBindingTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+using MvcRouteTester.Test.ApiControllers;
+using MvcRouteTester.Test.Assertions;
+
+using NUnit.Framework;
+
+namespace MvcRouteTester.Test.ApiRoute
+{
+	[TestFixture]
+	public class SimpleModelBindingTests
+	{
+		private HttpConfiguration config;
+
+		[SetUp]
+		public void MakeRouteTable()
+		{
+			RouteAssert.UseAssertEngine(new NunitAssertEngine());
+
+			config = new HttpConfiguration();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{data}");
+		}
+
+		[Test]
+		public void TestHasApiRouteToCorrectController()
+		{
+			var expectations = new
+			{
+				controller = "WithSimpleObject",
+				action = "Get"
+			};
+			RouteAssert.HasApiRoute(config, "/api/withsimpleobject/1-2", HttpMethod.Get, expectations);
+		}
+
+		[Test]
+		public void TestHasApiRouteParams()
+		{
+			var expectations = new
+				{
+					controller = "WithSimpleObject",
+					action = "Get",
+					data = "1-2"
+				};
+
+			RouteAssert.HasApiRoute(config, "/api/withsimpleobject/1-2", HttpMethod.Get, expectations);
+		}
+
+		[Test]
+		public void TestFluentMap()
+		{
+			config.ShouldMap("/api/withsimpleobject/1-2").To<WithSimpleObjectController>(HttpMethod.Get,
+				c => c.Get(new SimpleInputModel { X = 1, Y = 2 }));
+		}
+
+	}
+}
